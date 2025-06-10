b46e880f9f3f75928bcef96f8bec2fde77850acc
DivyaAhire11 <divyaahire121@gmail.com>
2025-06-10 18:39:42 +0530

    Second day added

---
+++ Day2/script.js
@@ -0,0 +1,20 @@
+
+var name="NAMAN";
+var name="Ritu"; //allowed
+name="Purva"; //allowed
+
+let age=26;
+//let age=30 is Error in the same scope
+age=30 //Allowed
+
+const PI = 3.14
+//PI = 3.14159 is Occured Error
+const user={
+    name:"john",
+    age:60
+}
+user.name = "Jane";  //Allowed
+
+
+
+
