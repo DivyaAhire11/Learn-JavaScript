4d4536a9b2b1b0dff9efa63a21803e998ff77a3f
DivyaAhire11 <divyaahire121@gmail.com>
2025-06-12 18:50:38 +0530

    Fourth Day added

---
+++ Day4/script.js
@@ -0,0 +1,52 @@
+//ARITHMETIC OPERATORS
+let num1 = 10 , num2 = 3
+console.log(num1 + num2); //13
+console.log(num1 - num2);  //7
+console.log(num1 * num2); //30
+console.log(num1 / num2); //3.333..
+console.log(num1 % num2); //1
+console.log(num1 ** num2); //1000
+
+
+//ASSIGNMENT OPERATORS
+
+let x = 5;
+console.log( x += 3 ); //x = x + 3 : 8
+console.log( x -= 2 );  //x = x - 2 : 6
+console.log( x *= 2 ); //x = x * 2 :12
+console.log( x /= 4 ); //x = x / 4 : 3
+console.log( x %= 2 ); //x = x % 2 :1
+console.log( x **=3 ); //x =  x**3 : 1
+ 
+
+//COMPARISON OPERATORS
+
+console.log( 5 == "5" ); //true
+console.log( 5 === "5" );  //false
+console.log( 5 != "5" ); //false
+console.log( 5 !== "5"  ); //true
+console.log( 7 > 4 ); //true
+console.log( 4 < 2 ); //false
+console.log( 5>= 5); //true
+console.log( 3 <= 2); //false
+
+//LOGICAL OPERATORS
+
+//  a    b     output(&&)
+//  T && T     TRUE
+//  T && F     FALSE
+//  F && T     FALSE
+//  F && F     FALSE
+
+
+//  a    b     output(||)
+//  T || T     TRUE
+//  T || F     TRUE
+//  F || T     TRUE
+//  F || F     FALSE
+
+ 
+
+
+
+
