8930b636adc539b3c979c2f5712de712d8d9e221
DivyaAhire11 <divyaahire121@gmail.com>
2025-06-13 18:00:43 +0530

    Day Five Added

---
+++ Day5/script.js
@@ -0,0 +1,67 @@
+let name = "Code"
+
+//console.log(name)
+
+let a = 10
+a += 3
+//console.log(a)
+
+let b = 20
+
+b -= 10
+
+//console.log(b)
+
+let c = 4
+
+c *= c
+
+//console.log(c)
+
+let d = 12
+
+d /= 2
+
+//console.log(d)
+
+let e = 12
+
+e %= 2
+
+//console.log(e)
+
+let p = 12
+let q = 12
+
+//console.log(p != q)
+
+let OTP=1234
+
+let user_otp = "1234"
+
+//console.log(OTP === user_otp)
+
+let axy = 10;
+
+let byz = 20;
+
+let res = axy < byz
+
+//console.log(res)
+
+let op = 20 >= 20
+
+//console.log(op)
+
+let opt = 40 <= 30
+
+//console.log(opt)
+
+let n1 = 20 , n2 = 2
+
+res = n1**n2
+
+//console.log(res)
+
+//console.log('z' > 'a') //true
+
