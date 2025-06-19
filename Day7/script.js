2b4cc4fedce34b592aa4a3f4b483917c126eb167
DivyaAhire11 <divyaahire121@gmail.com>
2025-06-19 12:59:22 +0530

    Day7 added

---
+++ Day7/script.js
@@ -0,0 +1,13 @@
+console.log(document);
+console.log(document.title);
+
+
+let newTitle = "welCome to easyCode";
+document.title = newTitle;
+console.log(document.title);
+console.log(document.body);
+
+let para  = "we learn about document keyword"
+document.writeln(para);
+document.write("<br/><b>using write method</b>")
+
