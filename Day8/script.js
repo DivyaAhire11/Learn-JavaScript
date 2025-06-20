255419313fa6c6edfd808331d3e15c82bd64cfb2
DivyaAhire11 <divyaahire121@gmail.com>
2025-06-20 14:44:54 +0530

    Day8 added

---
+++ Day8/script.js
@@ -0,0 +1,49 @@
+// alert("you are hacked");
+// alert(" Network slow");
+// let ans = confirm("Are you sure");
+// console.log(ans);
+
+  
+//  let res = confirm("Do you want to delete this photo?");
+//  if(res){
+//     alert("Photo deleted");
+//  }else{
+//     alert("Photo not deleted");
+//  }
+
+//  let userName = prompt("What is your Name"," Guest ");
+//  if(userName !== null){
+//    alert("WelCome"+ userName +"!");
+//  }else{
+//    alert("You Cancled the prompt!");
+//  }
+
+// let name= prompt("Enter Your Name :");
+// document.writeln("Hey HELLO :" + name + " WelCome to in our website ");
+
+// let responce = confirm("Do you want to play Game!");
+// console.log(responce);
+
+// let no1 = prompt("Enter first Number :");
+// let no2 = prompt("Enter your Second Number :");
+
+// let op1 = parseInt(no1);
+// let op2= parseInt(no2);
+// console.log("your input is", op1 ,"and",op2)
+// console.log("Addition :"+ (op1 + op2));
+// console.log("Subtraction :"+ (op1 - op2));
+// console.log("Multiplication :"+ (op1 * op2));
+// console.log("Division :"+ (op1 / op2));
+// console.log("Modulo :"+ (op1 % op2));
+
+
+let a = "Hello! "
+let b = "Good Night"
+
+console.log(a+b);
+
+
+let age = prompt("Enter your age ");
+console.log(typeof(age));  //string
+console.log( typeof parseInt(age)); //number
+console.log(typeof(age));  //string
