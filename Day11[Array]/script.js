d397074b6c7b4f45da4e54501aac0bf6623f0862
DivyaAhire11 <divyaahire121@gmail.com>
2025-06-22 22:51:18 +0530

    Day11 added

---
+++ Day11[Array]/script.js
@@ -0,0 +1,72 @@
+
+//   let days = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"];
+   
+//    console.log(days);
+//    console.log(days[0]);
+//    console.log(days[1]);
+//    console.log(days[2]);
+//    console.log(days[3]);
+//    console.log(days[4]);
+//    console.log(days[5]);
+//    console.log(days[6]);
+
+// let languages = ["JavaScript"];
+// console.log("Initialy array : ",languages)
+
+// languages.push("Python");
+// languages.push("Java");
+
+// console.log("after push method",languages);   // output = "JavaScript", "Python", "Java"
+
+// languages.unshift("C++");  //add first 
+// console.log("after unshift array:",languages) // output = "C++","JavaScript","Python","Java"
+
+// languages.shift();
+// console.log("after shift method: ",languages)
+ 
+// languages.pop();
+// console.log("aftre pop method :",languages)
+
+
+// let colors = []
+
+// colors.push("red")
+// colors.push("orange")
+// colors.push("white")
+
+// console.log(colors)
+
+
+// let numbers =[]
+
+// numbers.push(100)
+// numbers.push(200)
+// numbers.push(300)
+
+// console.log(numbers)
+
+// numbers.unshift("50")
+// numbers.unshift("10")
+// numbers.unshift(0)
+
+// console.log(numbers)
+
+
+
+
+let grade = ["A","B","C","D","E","F"]
+
+console.log(grade)
+
+grade.pop()
+
+grade.push("fail")
+grade.unshift("A+")
+
+
+console.log(grade)
+
+grade.shift()
+
+console.log(grade)
+
