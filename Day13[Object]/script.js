a4b8987a95af8f47d8970f26fb4842f2216c17fb
DivyaAhire11 <divyaahire121@gmail.com>
2025-06-25 21:59:26 +0530

    Day13 about object

---
+++ Day13[Object]/script.js
@@ -0,0 +1,51 @@
+// const person = {
+//   name: "Alice",
+//   age: 30,
+//   greet: function() {
+//     console.log("Hello!");
+//   }
+// };
+//  console.log(person.greet);
+//  console.log(person.age);
+//  console.log(person.name);
+
+// const car = {
+//   make: "Toyota",
+//   model: "Camry",
+//   year: 2020
+// };
+// console.log(car.make);
+//  console.log(car.model);
+//  console.log(car.year);
+
+
+// const book = {
+//   title: "1984",
+//   author: "George Orwell",
+//   pages: 328
+// };
+// console.log(book["title"]);
+//  console.log(book["author"]);
+//  console.log(book["pages"]);
+
+
+// const dog = {
+//   name: "Buddy",
+//   breed: "Golden Retriever",
+//   bark: function() {
+//     console.log("Woof!");
+//   }
+// };
+// console.log(dog["name"]);
+//  console.log(dog.bark);
+//  console.log(dog["breed"]);
+
+// const student = {
+//   name: "John",
+//   grade: "A",
+//   subjects: ["Math", "Science", "English"]
+// };
+
+// console.log(student.subjects);
+//  console.log(student["name"]);
+//  console.log(student.name);
