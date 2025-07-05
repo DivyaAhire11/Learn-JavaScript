9dc798d66cc504575ef9f95e6214f52c8eaaf66d
DivyaAhire11 <divyaahire121@gmail.com>
2025-07-05 23:32:59 +0530

    change

--- Day13[ObjectProperties]/script.js
+++ Day13[ObjectProperties]/script.js
@@ -8,17 +8,29 @@ let person = {
 // console.log(person);
 // console.log(person["age"]);
  
+
+//ADD NEW KEW:VALUE
+
 // person.city="Ambasan";
-person["city"]="Ambasan";
-console.log(person)  // change Origanal object
+// person["city"]="Ambasan";
+// console.log(person)  // change Origanal object
+
+
+// CHANGE VALUE
 
 // person.name="Divya Ahire";
 // person["name"]="Sayali Ahire";
 
 // person["About"]="Hey! I am Student"
 
+
+// DELETE KEY-VALUE
+
 // delete person.About;
 // delete person["About"];
+
+
+//CHECK KEY PRESEND IN OBJECT
 
 // let check= "isStudent" in person;
 // if(check){
@@ -27,5 +39,36 @@ console.log(person)  // change Origanal object
 //     console.log("not presnt Sorry");
 // }
  
-console.log(Object.keys(person))
-console.log(Object.values(person));
+
+// DISPLAY ALL KEYS
+
+// console.log(Object.keys(person));
+
+
+//DISPLAY ALL VALUE
+
+// console.log(Object.values(person));
+
+
+//DISPLAY ALL KEYS-VALUES(ENTRIES)
+// console.log(Object.entries(person));
+
+
+let Mobile ={
+    name : "OPPO",
+    price : 13000,
+    color : "purple"
+}
+
+Mobile.isHaveCharger=false;  // add 
+console.log(Mobile)
+
+let ObjectValues = Object.values(Mobile);
+// console.log(ObjectValues);  
+
+Mobile.brand = "OPPO"
+Mobile.is5G = true
+ 
+delete Mobile.brand;
+
+
