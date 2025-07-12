0594f7680fd9d0205af2a765b5eb00730a3fe750
DivyaAhire11 <divyaahire121@gmail.com>
2025-07-12 10:49:23 +0530

    String Methods

--- Day13[StringMethods]/script.js
+++ Day13[StringMethods]/script.js
@@ -4,11 +4,41 @@ let fruit = "Mango"
 
 // console.log(fruit.toUpperCase());
 
-let msg = "  Good Night   "
+let msg = "  Good Night  "
 
 // console.log(msg.trim()); 
 // console.log("hii",msg.trimStart() ,"hello");
 // console.log("hii",msg.trimEnd() ,"hello");
 
 // console.log(msg.replace("Night", "Morning"));
+
+let greet = "Hello! Good Morning , Have a nice Day Today"
+// console.log(greet)
+// console.log(greet.split(""));  // single letter saperate
+
+
+// console.log(greet.split(" "));  // word seperate
+// console.log(greet.split("")[0]); //return H
+// console.log(greet.split(" ")[1]); //return Good
+// console.log(greet.split(" ")[0]); //return Hello!
+
+
+
+// console.log(fruit.charAt(0))
+// console.log(fruit.charAt(4))
+// console.log(fruit.charAt(6))  //empty
+// console.log(fruit.charAt(1))
+
+// console.log("fruit".charAt(2))
+
+// console.log("hii".concat("Divya"));
+// console.log("hello" + "Ritu")
+
+console.log("JavaScript".includes("Java"))   // return True
+console.log("JavaScript".includes("hey"))    //return False
+
+console.log("JavaScript".indexOf("S"))    //return 4
+
+console.log("Hello Divya".replace("Divya", "Ritu"))  //Hello Ritu  
+
 
