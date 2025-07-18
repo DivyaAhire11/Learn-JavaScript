a22cab34aaf88e34a6587ccdd1ba55bfdebae154
DivyaAhire11 <divyaahire121@gmail.com>
2025-07-18 23:05:05 +0530

    about loops in JS

--- Day14[Loop]/script.js
+++ Day14[Loop]/script.js
@@ -5,39 +5,34 @@
 //     a++;
 // }
 
-let colors = ["red", "green", "blue", "purple"];
-let index = 0;
-while (index < colors.length) {
-    console.log(index, colors[index]);
-    index++;
-}
+// let colors = ["red", "green", "blue", "purple"];
+// let index = 0;
+// while (index < colors.length) {
+//     console.log(index , colors[index]);
+//     index++;
+// }
 
-let sum = 0;
-let i = 1;
-while (i <= 100) {
-    sum += i;
-    i++;
-}
-console.log(sum);
-
+// let sum = 0, i = 1;
+// while (i <= 10) {
+//   sum += i;
+//   i++;
+// }
+// console.log("sum is :", sum);
 
 
-let age = 0;
-while (age < 1 || age > 120) {
-    age = parseInt(prompt("Enter valid age:"));
-}
+// let age = 0;
+// while (age < 1 || age > 120) {
+//   age = parseInt(prompt("Enter valid age:"));
+// }
+// console.log("Your age is:", age);
 
 
-let str = "JavaScript";
-let idex = 0;
-while (idx < str.length) {
-  console.log(str[idx]);
-  idx++;
-}
+// let str = "JavaScript";
+// let idx = 0;
+// while (idx < str.length) {
+//   console.log(str[idx]);
+//   idx++;
+// }
 
 
-let val = 1;
-while (val <= 1024) {
-  console.log(val);
-  val *= 2;
-}
+
---
+++ Day15[loopPart2]/loops.js
@@ -0,0 +1,26 @@
+//find odd number
+// for(i=0;i<21;i++){
+//     if(i%2){
+//         console.log(i)
+//     }
+// }
+
+
+// Use a for loop to count vowels in a string like "javascript"
+let str = "javascript", count = 0;
+for (let i = 0; i < str.length; i++) {
+    let x = str[i];
+    if (x === "a" || x === "e" || x === "i" || x === "o" || x === "u") {
+        count++;
+        console.log(x);
+    }
+}
+console.log("total vowels is : ", count);
+
+
+// Use a for loop to reverse the characters in a string
+let Name = "Manas"
+for (let x = Name.length; x >= 0; x--) {
+    console.log(Name[x]);
+}
+
